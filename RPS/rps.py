3ba449977c3872433ba110382d9deb178502142b
Tyler <126568874+1313Revan@users.noreply.github.com>
2024-03-04 12:42:40 -0500

    Updated art variables and moved file
    Rewrote art variable imports and moved the file into its respective directory.

--- RPS/rps.py
+++ RPS/rps.py
@@ -1,7 +1,7 @@
 # This is a project from Angela Wu's 100 Days of Code Python bootcamp
 
 import random
-import art_vars
+import rps_art as art
 
 
 print('\nWelcome to the ROCK PAPER SCISSORS terminal game!\nYou\'ll be placed against the computer to see which of you comes out on top.\n')
@@ -10,43 +10,43 @@ print('1. Rock\n2. Paper\n3. Scissors\n')
 
 player = input('>> ').lower()
 if player == 'rock' or player == '1':
-    p_choice = art_vars.rock
+    p_choice = art.rock
 elif player == 'paper' or player == '2':
-    p_choice = art_vars.paper
+    p_choice = art.paper
 else:
-    p_choice = art_vars.scissors
+    p_choice = art.scissors
 
 computer = random.randint(1,3)
 if computer == 1:
-    c_choice = art_vars.rock
+    c_choice = art.rock
 elif computer == 2:
-    c_choice = art_vars.paper
+    c_choice = art.paper
 else:
-    c_choice = art_vars.scissors
+    c_choice = art.scissors
 
 print('\n********************')
 print('\nYou chose:\n',p_choice,'\n')
 print('\nComputer chose:\n',c_choice)
 print('\n********************')
 
-if p_choice == art_vars.rock:
-    if c_choice == art_vars.rock:
+if p_choice == art.rock:
+    if c_choice == art.rock:
         print('\nDRAW\n')
-    elif c_choice == art_vars.paper:
+    elif c_choice == art.paper:
         print('\nYOU LOSE\n')
     else:
         print('\nYOU WIN\n')
-elif p_choice == art_vars.paper:
-    if c_choice == art_vars.rock:
+elif p_choice == art.paper:
+    if c_choice == art.rock:
         print('\nYOU WIN\n')
-    elif c_choice == art_vars.paper:
+    elif c_choice == art.paper:
         print('\nDRAW\n')
     else:
         print('\nYOU LOSE')
 else:
-    if c_choice == art_vars.rock:
+    if c_choice == art.rock:
         print('\nYOU LOSE')
-    elif c_choice == art_vars.paper:
+    elif c_choice == art.paper:
         print('\nYOU WIN\n')
     else:
         print('\nDRAW\n')
