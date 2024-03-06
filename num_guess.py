770cf07479e679d43fb68e23a15e9f427a0b5625
Tyler <126568874+1313Revan@users.noreply.github.com>
2024-03-06 12:29:38 -0500

    Added num_guess.py

---
+++ num_guess.py
@@ -0,0 +1,64 @@
+# This is a project from Angela Wu's 100 Days of Code Python bootcamp
+
+from random import randint
+
+
+def difficulty():
+    print("\nSelect your difficulty:")
+    print("\n1. Easy\n2. Medium\n3. Hard\n")
+    diff = input(">> ").lower()
+    if diff == "1" or diff == "easy":
+        tries = 15
+    elif diff == "2" or diff == "medium":
+        tries = 10
+    elif diff == "3" or diff == "hard":
+        tries = 5
+    else:
+        print("\nInvalid option.\n")
+        difficulty()
+    return tries
+
+def game(difficulty_level):
+    answer = randint(1,100)
+    lives = difficulty_level
+    print("\nI'm thinking of a number between 1 and 100.\nCan you guess it?\n")
+    end = False
+    while not end:
+        print(f"Lives remaining: {lives}\n")
+        guess = int(input(">> "))
+        if guess == answer:
+            end = True
+        elif guess > answer:
+            print("\nToo high!\n")
+            lives -= 1
+        elif guess < answer:
+            print("\nToo low!\n")
+            lives -= 1
+    if lives > 0:
+        print(f"\nThe answer was {answer}, good job!\n")
+    elif lives == 0:
+        print(f"\nSorry, you ran out of lives. The answer was {answer}.\n")
+    print("\nWould you like to play again?\n")
+    play_again = input(">> ").lower()
+    if play_again == "yes" or play_again == "y":
+        game(difficulty())
+    else:
+        print("\nGoodbye!\n")
+        exit()
+
+def menu():
+    print("\nWelcome to NUMBER GUESS!\n")
+    print("\nPlease select an option:")
+    print("\n1. Start\n2. Quit\n")
+    choice = input(">> ").lower()
+    if choice == "1" or choice == "start":
+        game(difficulty())
+    elif choice == "2" or choice == "quit":
+        print("\nGoodbye!\n")
+        exit()
+    else:
+        print("\nInvalid option.\n")
+        menu()
+
+
+menu()
